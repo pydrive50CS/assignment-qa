3cc8b9436c902b882b6817997f4d5dd80bc37a63
pydrive50CS <pydriveupdater@gmail.com>
2025-07-13 18:00:16 +0545

    db changed to sqlite as requirement

--- src/main/java/utilities/DBUtility.java
+++ src/main/java/utilities/DBUtility.java
@@ -6,36 +6,43 @@ import java.util.List;
 
 public class DBUtility {
 
-    public static Connection getConnection() throws Exception {
-        String url = ConfigReader.get("db_url");
-        String user = System.getenv("DB_USERNAME");
-        String password = System.getenv("DB_PASSWORD");
-        return DriverManager.getConnection(url, user, password);
+    private static final String DB_URL = "jdbc:sqlite:src/test/resources/testdb.sqlite";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(DB_URL);
     }
 
-    public static List<Object[]> getLoginData() throws Exception {
+    public static List<Object[]> getLoginData() {
         List<Object[]> data = new ArrayList<>();
+        String query = "SELECT test_case_code, test_case_name, username, password, expected_result FROM users";
+
         try (Connection conn = getConnection();
              Statement stmt = conn.createStatement();
-             ResultSet rs = stmt.executeQuery("SELECT test_case_code, test_case_name, username, password, expected_result FROM users")) {
+             ResultSet rs = stmt.executeQuery(query)) {
 
             while (rs.next()) {
                 String testCode = rs.getString("test_case_code");
                 String testCaseName = rs.getString("test_case_name");
                 String user = rs.getString("username");
                 String pass = rs.getString("password");
-                boolean result = rs.getBoolean("expected_result");
+                boolean result = rs.getInt("expected_result") == 1;
                 data.add(new Object[]{testCode, testCaseName, user, pass, result});
             }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
+
         return data;
     }
 
-    public static List<Object[]> getRequiredFieldTestData() throws Exception {
+    public static List<Object[]> getRequiredFieldTestData() {
         List<Object[]> data = new ArrayList<>();
+        String query = "SELECT test_case_code, description, first_name, last_name, mobile, gender, expect_error FROM form_test_cases";
+
         try (Connection conn = getConnection();
              Statement stmt = conn.createStatement();
-             ResultSet rs = stmt.executeQuery("SELECT test_case_code, description, first_name, last_name, mobile, gender, expect_error FROM form_test_cases")) {
+             ResultSet rs = stmt.executeQuery(query)) {
 
             while (rs.next()) {
                 String testCode = rs.getString("test_case_code");
@@ -44,11 +51,14 @@ public class DBUtility {
                 String last_name = rs.getString("last_name");
                 String mobile = rs.getString("mobile");
                 String gender = rs.getString("gender");
-                boolean expect_error = rs.getBoolean("expect_error");
+                boolean expect_error = rs.getInt("expect_error") == 1;
                 data.add(new Object[]{testCode, description, first_name, last_name, mobile, gender, expect_error});
             }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
+
         return data;
     }
 }
-
